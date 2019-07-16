83a62f0b25d5f5acd0b2c5764d97470f2ca884a3
nandang <nandanghermawan21@gmail.com>
2019-07-16 11:04:16 +0700

    fixed untracked files

---
+++ lib/util/FontUtil.dart
@@ -0,0 +1,6 @@
+class FontUtil {
+  static const String primary = "Champagne-Limousines";
+  static const String scondary = "MRF-Lemonberry-Sans";
+  static const double xxl = 25;
+  static const double xl = 22;
+}
